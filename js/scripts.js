e2d91596f5f13ccc7804a66dcabcfd5a1bddc3e0
cobaltgalaxy <CJ-MACBOOK@Charless-MacBook-Pro.local>
2015-02-12 22:26:51 -0600

    Added Problem 2

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,27 @@
 
 // Dog Years
 
-var sparkyAge = prompt("Please tell us Sparky's age.")
+var sparkyAge = prompt("Please tell us Sparky's age.");
 
 var dogYears = sparkyAge * 7;
 
+console.log("PROBLEM 1: DOG YEARS");
 console.log("Sparky is " + sparkyAge + " human years old which is " + dogYears + " in dog years");
 
 
 // Slice of Pie part 1
+console.log("PROBLEM 2: SLICE OF PIE PART 1");
 
+var noPizzas = prompt("How many pizzas are being ordered?");
+var noPeople = prompt("How many people will be eating?");
+
+
+var noSlices = 12;
+var slicesPp = noPizzas * noSlices / noPeople;
+
+
+
+
+console.log("There are " + noSlices + " per pizza.");
+
+console.log("Based on the number of people and pizzas with 12 slices. Each person can have " + slicesPp + " slices each.");
