a2219dbf0c7fe5cae81977fc5540e6b31bc1b9e3
cobaltgalaxy <CJ-MACBOOK@Charless-MacBook-Pro.local>
2015-02-12 22:15:45 -0600

    Add Files

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+/**
+ * Charles Johnson
+ * Expressions Worksheet
+ * SDI Section 1
+ * February 12, 2015
+ */
+
+// Dog Years
+
+var sparkyAge = prompt("Please tell us Sparky's age.")
+
+var dogYears = sparkyAge * 7;
+
+console.log("Sparky is " + sparkyAge + " human years old which is " + dogYears + " in dog years");
+
+
+// Slice of Pie part 1
+
