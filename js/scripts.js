a0f8ddb7bae0b57903187324b7facbae20df35a1
cobaltgalaxy <CJ-MACBOOK@Charless-MacBook-Pro.local>
2015-02-12 22:49:17 -0600

    Added Problem 4

--- js/scripts.js
+++ js/scripts.js
@@ -31,3 +31,41 @@ var slicesPp = noPizzas * noSlices / noPeople;
 console.log("There are " + noSlices + " per pizza.");
 
 console.log("Based on the number of people and pizzas with 12 slices. Each person can have " + slicesPp + " slices each.");
+
+
+// Slice of Pie part 2
+console.log("PROBLEM 3: SLICE OF PIE PART 2");
+
+var noPizzas = prompt("How many pizzas are being ordered?");
+var noPeople = prompt("How many people will be eating?");
+
+
+var noSlices = 12;
+var slicesPp = noPizzas * noSlices / noPeople;
+
+
+
+
+console.log("There are " + noSlices + " per pizza.");
+
+console.log("Based on the number of people and pizzas with 12 slices. Each person can have " + slicesPp + " slices each.");
+
+
+// Average Shopping Bill
+
+console.log("PROBLEM 4: AVERAGE SHOPPING BILL");
+
+
+var groceryTotals = [prompt("Enter Week 1 Grocery Amount"), prompt("Enter Week 2 Grocery Amount"), prompt("Enter Week 3 Grocery Amount"), prompt("Enter Week 4 Grocery Amount"), prompt("Enter Week 5 Grocery Amount")];
+
+var totalGrocery = groceryTotals[0] + groceryTotals[1] + groceryTotals[2] + groceryTotals[3] + groceryTotals[4];
+
+var averageSpending = (groceryTotals[0] + groceryTotals[1] + groceryTotals[2] + groceryTotals[3] + groceryTotals[4]) / 5;
+
+
+console.log("You have spent a total of $" + totalGrocery + " on groceries over 5 weeks. That is an average of $" + averageSpending " per week");
+
+
+
+
+
